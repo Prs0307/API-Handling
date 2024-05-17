8f527f2ae8ef0ac44e523ffe6a48a215fed39c2c
Prince Sahu <princesahu169@gmail.com>
2024-05-17 11:19:08 +0530

    updating proxy

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -3,21 +3,43 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 
 
+
 function App() {
   const [products,setproducts]=useState([])
+  const[loading,setLoading]=useState(false)
+  const [error,seterror]=useState(false)
   useEffect(()=>{
     // eslint-disable-next-line no-extra-semi
     ;(async()=>{
-      const response = await axios.get('/api/products')
-      console.log(' data :'+response.data)
+      try {
+        setLoading(true)
+        seterror(false)
+        const response = await axios.get('/api/products')
+        console.log(' data :'+response.data)
+        setproducts(response.data)
+        setLoading(false)
+      } catch (error) {
+        seterror(true)
+        setLoading(false)
+      }
     })()
   },[])
+
+  if(error) {
+    console.log('something went wrong....');
+    // return <h1>error occurred :  ...</h1>
+
+
+  }
+  // if(loading){
+  //   return <h1>loading ...</h1>
+  // }
   
 
   return (
     <>
     <h1>
-      API CALL react 
+      API Call in  react 
     </h1>
     <h2>No of products : {products.length}</h2>
     </>
